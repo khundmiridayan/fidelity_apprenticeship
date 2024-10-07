4373bc68df274ddc95be1ca3e8d45885cc6b5dda
Dayan Khundmiri <70951021+khundmiridayan@users.noreply.github.com>
2024-10-07 16:27:06 +0530

    Day 11 assement questions 1 and 2

--- Question1.java
+++ Question1.java
@@ -1,46 +1,109 @@
 import java.util.Scanner;
 
+class Node {
+    String data;
+    Node next;
 
-class LinkedList{
+    public Node(String data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// LinkedList class to manage the linked list operations
+class LinkedList {
     Node head;
 
-    public void insertAtTail(int data){
+    // Method to add elements to the linked list
+    public void append(String data) {
         Node newNode = new Node(data);
-        if(head == null){
+        if (head == null) {
             head = newNode;
-        }else {
-            Node current = head;
-            while(current.next != null){
-                current = current.next;
-            }
-            current.next = newNode;
+            return;
         }
+        Node last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = newNode;
     }
-public void display(){
-    Node current = head;
-    while(current != null){
-    System.out.println(current.date);
-    current = current.next;
-    }
-}
-}
 
+    // Method to remove an element at a particular index
+    public void removeAtIndex(int index) {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+
+        // If the head needs to be removed
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+
+        // Find the previous node of the node to be deleted
+        for (int i = 0; temp != null && i < index - 1; i++) {
+            temp = temp.next;
+        }
+
+        // If index is out of range
+        if (temp == null || temp.next == null) {
+            System.out.println("No element at index " + index);
+            return;
+        }
+
+        // Node temp.next is the node to be deleted
+        Node next = temp.next.next;
+
+        // Unlink the node from the linked list
+        temp.next = next;
+    }
+
+    // Method to print the linked list
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("None");
+    }
+}
 
 public class Question1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        LinkedList list = new LinkedList();
-        System.out.println("Enter the number of elements in the linked list: ");
-        int n = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
 
-        for(int i = 0; i < n; i++){
-            System.out.println("enter data for node" + (i +1) + ":");
-            int data = scanner.nextInt();
-            list.insertAtTail(data);
+        // Create a linked list
+        LinkedList llist = new LinkedList();
+
+        // Input the number of elements for the linked list
+        System.out.print("Enter the number of elements for the linked list: ");
+        int n = sc.nextInt();
+        sc.nextLine(); // Consume the newline
+
+        // Get elements from the user and add to the linked list
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            String element = sc.nextLine();
+            llist.append(element);
         }
 
-        System.out.println("The Linked List is: ");
-        list.display();
-        scanner.close();
+        // Print the list before deletion
+        System.out.println("Linked list before deletion:");
+        llist.printList();
+
+        // Get the index from the user to remove an element
+        System.out.print("Enter the index of the element to remove: ");
+        int index = sc.nextInt();
+
+        // Remove the element at the specified index
+        llist.removeAtIndex(index);
+
+        // Print the list after deletion
+        System.out.println("Linked list after deletion:");
+        llist.printList();
     }
 }
